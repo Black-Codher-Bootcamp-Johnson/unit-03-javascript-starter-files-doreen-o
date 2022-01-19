e5ebe2f8ccef408972a406404728f4072b671d71
doreen.o <doreenotieno21@gmail.com>
2022-01-19 20:51:23 +0000

    session 3 tasks

--- session-3/index.js
+++ session-3/index.js
@@ -1,7 +1,30 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-const a = 5;
-const b = 10;
-let c = a + b;
+let a = 5;
+let b = 10;
+const c = a + b;
 
 console.log(c);
+
+function sayHey() {
+    console.log("Hey!");
+}
+sayHey();
+
+function conversation() {
+    sayHey();
+    console.log("How are you?");
+    console.log("Goodbye");
+}
+conversation();
+
+function futureAge(x,y) {
+    console.log(x)
+    console.log(y)
+    const answer = x + 5;
+    console.log(y + " Your age in 5 years time will be..." + answer)
+    return y + " Your age in 5 years time will be.." + answer;
+}
+
+futureAge(22, "Mary")
+
