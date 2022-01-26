6e7eb5c28561f24ba7b33eab09dccf5540bcac4f
doreen.o <doreenotieno21@gmail.com>
2022-01-26 20:52:46 +0000

    task 2 switch

--- session-5/index.js
+++ session-5/index.js
@@ -1,21 +1,50 @@
+// function getGrade(total){
+//     console.log(total)
+//     if(total > 90){
+//     console.log("Youre grade is A+");
+//     } else if (total > 80 && total<= 90){
+//         console.log("You're grade is A")
+//         }else if (total > 70 && total<= 80){
+//             console.log("You're grade is B")
+//             }else if (total > 60 && total<= 70){
+//                 console.log("You're grade is C")
+//                 }else if (total > 50 && total<= 60){
+//                     console.log("You're grade is D")
+//                     }else if (total > 40 && total<= 50){
+//                         console.log("You're grade is E")
+//                         }else if (total > 30 && total<= 40){
+//                             console.log("You're grade is F")
+//                             }else if (total< 30){
+//                                 console.log("You're grade is Fail")
+//                                 }
+// }
+// getGrade(55)
+
 function getGrade(total){
-    console.log(total)
-    if (total > 90){
-    console.log("Youre grade is A+");
-    } else if (total > 80 && total<= 90){
+// let total;
+    switch (true){
+    case (total >90):
+    console.log("Youre grade is A+")
+    break;
+        case (total > 80):
         console.log("You're grade is A")
-        }else if (total > 70 && total<= 80){
+        break;
+        case(total > 70):
             console.log("You're grade is B")
-            }else if (total > 60 && total<= 70){
+            break;
+            case (total > 60):
                 console.log("You're grade is C")
-                }else if (total > 50 && total<= 60){
+                break;
+                case (total > 50):
                     console.log("You're grade is D")
-                    }else if (total > 40 && total<= 50){
+                    break;
+                    case (total > 40):
                         console.log("You're grade is E")
-                        }else if (total > 30 && total<= 40){
+                        break;
+                        case (total > 30):
                             console.log("You're grade is F")
-                            }else if (total< 30){
+                            break;
+                            default:
                                 console.log("You're grade is Fail")
-                                }
-}
-getGrade(55)
+                        }
+                    }
