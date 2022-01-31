f1f69ced6bf6d518143f11b902690209d5b669ae
doreen.o <doreenotieno21@gmail.com>
2022-01-31 20:37:03 +0000

    task 2

--- session-6/index.js
+++ session-6/index.js
@@ -27,7 +27,11 @@ console.log("Random Number Generated:" + randomNumber);
 
 // ****************************************************TASK 2****************************************************
 
-
+for(let counter =1; counter <=10; counter = counter + 1){
+    const randomNumber = Math.round(Math.random() * 10)
+    console.log("counter2", counter)
+    console.log("Random number generated:" + randomNumber);
+}
 
 
 
